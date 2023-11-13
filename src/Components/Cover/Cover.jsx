7533fe3467346fce8f3fcd09c6063c958cd2d0b6
oluwadydx <arogundadeadedoyin@gmail.com>
2023-11-13 14:54:36 +0100

    updated cover

--- src/Components/Cover/Cover.jsx
+++ src/Components/Cover/Cover.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Cover.css'
+import CoverImage from '../../Images/musicIcon.svg'
 
 const Cover = () => {
   return (
-    <div>Cover</div>
+    <div className='Cover'>
+       <img src={CoverImage} alt="Description" />
+    </div>
+   
   )
 }
 
