38fff625123b22c4f0eb5a578befc2fdb0806760
Daniel Ntichika <kevindaniel4ever@gmail.com>
2023-11-16 12:33:40 +0100

    Updated data, added more components and functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/NavBar/Navbar";
 import Home from "./Components/Home/Home";
 import Play from "./Components/Play/Play";
@@ -6,20 +6,43 @@ import { MyContext } from "./Data";
 import { data } from "./Data";
 import Cover from "./Components/Cover/Cover";
 import BackgroundPlay from "./Components/BackgroundPlay/BackgroundPlay";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Queue from "./Components/Queue/Queue";
+import Video from "./Components/Video/Video";
+import Playlist from "./Components/PlayList/Playlist";
+import Album from "./Components/Albums/Album";
+import Nowplaying from "./Components/NowPlaying/Nowplaying";
+
 
 function App() {
+  const [getIndex, setGetIndex] = useState(0);
+
+  const handleGetIndex = (index) => {
+    setGetIndex(index);
+  };
+
   return (
-    <MyContext.Provider value={data}>
-      <main className="main_container">
-        {/* <Cover /> */}
-        <div className="main_body">
-          <Navbar />
-          <Home />
-        </div>
-        {/* <BackgroundPlay /> */}
-        <Play />
-      </main>
-    </MyContext.Provider>
+    <BrowserRouter>
+      <MyContext.Provider value={data}>
+        <main className="main_container">
+          {/* <Cover /> */}
+          <div className="main_body">
+            <Navbar />
+            <Routes>
+              <Route path="/" Component={Home}/>
+              <Route path="/queue" Component={Queue}/>
+              <Route path="/video" Component={Video}/>
+              <Route path="/playlist" Component={Playlist}/>
+              <Route path="/album" Component={Album}/>
+              <Route path="/nowplaying" element={<Nowplaying getIndex={getIndex} />}/>
+            </Routes>
+            {/* <Home handleSongClick={handleSongClick} /> */}
+          </div>
+          {/* <BackgroundPlay /> */}
+          <Play getIndex={handleGetIndex} />
+        </main>
+      </MyContext.Provider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Components/PlayList/Playlist.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Playlist.css'
+
+const Playlist = () => {
+  return (
+    <div className='playlist_container'>
+        <h3>No Playlist Found</h3>
+    </div>
+  )
+}
+
+export default Playlist
