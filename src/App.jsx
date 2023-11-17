fb91c49c8f3b0c47a272937f01c6fb5256389e06
iamYehhmii <franciscoabhulimen@gmail.com>
2023-11-17 08:58:13 +0100

    V.1.0 updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Components/NavBar/Navbar";
 import Home from "./Components/Home/Home";
 import Play from "./Components/Play/Play";
@@ -17,33 +17,44 @@ import Nowplaying from "./Components/NowPlaying/Nowplaying";
 
 function App() {
   const [getIndex, setGetIndex] = useState(0);
+  const [firstLoad, setFirstLoad] = useState(true);
 
   const handleGetIndex = (index) => {
     setGetIndex(index);
   };
+
+  useEffect(() => {
+    const delay = 3000;
+
+    const timer = setTimeout(() => {
+      setFirstLoad(false);
+    }, delay);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <BrowserRouter>
       <MyContext.Provider value={data}>
         <MyProvider>
         <main className="main_container">
-            {/* <Cover /> */}
-            <div className="main_body">
-              <Navbar />
-              <Routes>
-              <Route path="/" Component={Home}/>
-              <Route path="/queue" Component={Queue}/>
-              <Route path="/video" Component={Video}/>
-              <Route path="/playlist" Component={Playlist}/>
-              <Route path="/album" Component={Album}/>
-              <Route path="/nowplaying" element={<Nowplaying getIndex={getIndex} />}/>
-            </Routes>
-            {/* <Home handleSongClick={handleSongClick} /> */}
-            </div>
-            {/* <BackgroundPlay /> */}
-            <Play getIndex={handleGetIndex} />
-          </main>
-        </MyProvider>
+          {firstLoad ? (
+            <Cover />
+          ) : (
+            <><div className="main_body">
+                  <Navbar />
+                  <Routes>
+                    <Route path="/" Component={Home} />
+                    <Route path="/queue" Component={Queue} />
+                    <Route path="/video" Component={Video} />
+                    <Route path="/playlist" Component={Playlist} />
+                    <Route path="/album" Component={Album} />
+                    <Route path="/nowplaying" element={<Nowplaying getIndex={getIndex} />} />
+                  </Routes>
+                </div><Play getIndex={handleGetIndex} /></>
+          )}
+        </main>
+      </MyProvider>
     </MyContext.Provider>
     </BrowserRouter>
   );
